58878a759927357f8ecb375b20b44c4e103df566
dongyan <dybest@icloud.com>
2020-10-28 14:38:05 +0800

    implement

---
+++ main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"regexp"
+	"time"
+
+	"github.com/google/go-github/v32/github"
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+var (
+	inspect bool
+	token   string
+	owner   string
+	repo    string
+	mentor  string
+
+	ctx = context.Background()
+	cli *github.Client
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+	flag.BoolVar(&inspect, "inspect", false, "Inspect issues")
+	flag.StringVar(&token, "token", "", "GitHub personal access token")
+	flag.StringVar(&owner, "owner", "dyzsr", "Owner of repo")
+	flag.StringVar(&repo, "repo", "issuepublic", "Repo name")
+	flag.StringVar(&mentor, "mentor", "lzmhhh123", "Mentor of the issue")
+}
+
+func initCli() {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	cli = github.NewClient(tc)
+}
+
+type labelOption struct {
+	and []string
+	or  []string
+	nor []string
+}
+
+func getIssuesByLabels(owner string, repo string, opt *labelOption) ([]*github.Issue, error) {
+	log.Printf("start getIssuesByLabels")
+	defer log.Printf("end getIssuesByLabels")
+
+	if opt == nil {
+		opt = &labelOption{}
+	}
+
+	var allIssues []*github.Issue
+	nextPage := 1
+	for nextPage > 0 {
+		listOpt := &github.IssueListByRepoOptions{
+			Labels: opt.and,
+		}
+		listOpt.Page = nextPage
+		issues, resp, err := cli.Issues.ListByRepo(ctx, owner, repo, listOpt)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		log.Printf("got: %v\n", issueNumbers(issues))
+		allIssues = append(allIssues, issues...)
+		nextPage = resp.NextPage
+	}
+
+	var ret []*github.Issue
+	for _, issue := range allIssues {
+		var include, exclude bool
+		for _, lb := range opt.or {
+			for _, label := range issue.Labels {
+				if lb == *label.Name {
+					include = true
+					break
+				}
+			}
+		}
+		if len(opt.or) == 0 {
+			include = true
+		}
+		for _, lb := range opt.nor {
+			for _, label := range issue.Labels {
+				if lb == *label.Name {
+					exclude = true
+					break
+				}
+			}
+		}
+		if include && !exclude {
+			ret = append(ret, issue)
+		}
+	}
+
+	return ret, nil
+}
+
+func printIssue(issue *github.Issue) {
+	fmt.Printf("#%d:\n", *issue.Number)
+	fmt.Printf("\tname: %s\n", *issue.Title)
+	fmt.Printf("\turl: %s\n", *issue.HTMLURL)
+	fmt.Printf("\tlabels: %s\n", labelNames(issue.Labels))
+}
+
+func issueNumbers(issues []*github.Issue) []int {
+	ret := make([]int, 0, len(issues))
+	for _, issue := range issues {
+		ret = append(ret, *issue.Number)
+	}
+	return ret
+}
+
+func labelNames(labels []*github.Label) []string {
+	ret := make([]string, 0, len(labels))
+	for _, label := range labels {
+		ret = append(ret, *label.Name)
+	}
+	return ret
+}
+
+func appendLabels(labels []*string, newLabels ...string) []*string {
+	for _, label := range newLabels {
+		labels = append(labels, &label)
+	}
+	return labels
+}
+
+func findLabel(labels []string, target string) bool {
+	for _, label := range labels {
+		if target == label {
+			return true
+		}
+	}
+	return false
+}
+
+func editDesc(desc string, slackChannel string) (string, error) {
+	matched, err := regexp.MatchString("(?m)^## Description", desc)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+	ret := desc
+	if !matched {
+		ret = "## Description\n" + ret
+	}
+	//matched, err = regexp.MatchString("(?s)(?m)^## SIG slack channel.*^## Score.*^## Mentor", desc)
+	//if err != nil {
+	//	return "", errors.WithStack(err)
+	//}
+	//if !matched {
+	ret += fmt.Sprintf(`
+## SIG slack channel
+
+ %s
+
+## Score
+
+300
+
+## Mentor
+
+- @%s
+`, slackChannel, mentor)
+	//}
+
+	return ret, nil
+}
+
+var (
+	include     = []string{"type/bug"}
+	exclude     = []string{"high-performance", "challenge-program"}
+	addToIssues = []string{"challenge-program", "status/help-wanted"}
+	sigLabels   = []string{"sig/execution", "sig/planner"}
+	sigs        = map[string]string{
+		"sig/execution": "[#sig-exec](https://slack.tidb.io/invite?team=tidb-community&channel=sig-exec&ref=high-performance)",
+		"sig/planner":   "[#sig-planner](https://slack.tidb.io/invite?team=tidb-community&channel=sig-planner&ref=high-performance)",
+	}
+)
+
+func defaultEditIssue(issue *github.Issue) (err error) {
+	printIssue(issue)
+	if inspect {
+		return nil
+	}
+
+	var desc string
+	if issue.Body != nil {
+		desc = *issue.Body
+	}
+
+	newDesc := desc
+	labels := labelNames(issue.Labels)
+	for label, sig := range sigs {
+		if findLabel(labels, label) {
+			newDesc, err = editDesc(desc, sig)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			break
+		}
+	}
+
+	labels = append(labels, addToIssues...)
+
+	cli.Issues.Edit(ctx, owner, repo, *issue.Number, &github.IssueRequest{
+		Title:  issue.Title,
+		Body:   &newDesc,
+		Labels: &labels,
+	})
+	return nil
+}
+
+type editOption struct {
+	labelOption
+	editIssue func(*github.Issue) error
+}
+
+func editIssues(opt *editOption) error {
+	log.Printf("start editIssues")
+	defer log.Printf("end editIssues")
+	issues, err := getIssuesByLabels(owner, repo, &opt.labelOption)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	for _, issue := range issues {
+		if err := opt.editIssue(issue); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	initCli()
+
+	editOpt := &editOption{
+		labelOption: labelOption{
+			and: include,
+			or:  sigLabels,
+			nor: exclude,
+		},
+		editIssue: defaultEditIssue,
+	}
+	err := editIssues(editOpt)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/google/go-github/v32/github"
+)
+
+var (
+	writeDesc bool
+)
+
+func TestCreateIssues(t *testing.T) {
+	title := "Issue on " + time.Now().String()
+	var body string
+	if writeDesc {
+		body = `
+## Description
+...
+
+## Bug Report
+
+These sql results are inconsistent with MySQL
+
+### 1. Minimal reproduce step (Required)
+...
+
+### 2. What did you expect to see? (Required)
+...
+
+### 3. What did you see instead (Required)
+...
+
+### 4. What is your TiDB version? (Required)
+...
+	`
+	}
+	req := &github.IssueRequest{
+		Title: &title,
+		Body:  &body,
+	}
+	issue, _, err := cli.Issues.Create(ctx, "dyzsr", "issuepublic", req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("#%d, title:\"%s\"\n", *issue.Number, *issue.Title)
+}
+
+func TestAddLabels(t *testing.T) {
+	editOpt := &editOption{
+		labelOption: labelOption{},
+		editIssue: func(issue *github.Issue) error {
+			var lb string
+			switch rand.Intn(5) {
+			case 0:
+				lb = "type/bug"
+			case 1:
+				lb = "high-performance"
+			case 2:
+				lb = "challenge-program"
+			case 3:
+				lb = "sig/execution"
+			case 4:
+				lb = "sig/planner"
+			}
+			labels, _, err := cli.Issues.AddLabelsToIssue(ctx, owner, repo, *issue.Number, []string{lb})
+			if err != nil {
+				return err
+			}
+			fmt.Printf("#%d, labels: %s\n", *issue.Number, labelNames(labels))
+			return nil
+		},
+	}
+	err := editIssues(editOpt)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRemoveLabels(t *testing.T) {
+	editOpt := &editOption{
+		labelOption: labelOption{},
+		editIssue: func(issue *github.Issue) error {
+			fmt.Printf("#%d\n", *issue.Number)
+			_, err := cli.Issues.RemoveLabelsForIssue(ctx, owner, repo, *issue.Number)
+			return err
+		},
+	}
+	err := editIssues(editOpt)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCloseIssues(t *testing.T) {
+	editOpt := &editOption{
+		labelOption: labelOption{},
+		editIssue: func(issue *github.Issue) error {
+			fmt.Printf("#%d\n", *issue.Number)
+			state := "close"
+			_, _, err := cli.Issues.Edit(ctx, owner, repo, *issue.Number, &github.IssueRequest{State: &state})
+			return err
+		},
+	}
+	err := editIssues(editOpt)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	flag.BoolVar(&writeDesc, "desc", false, "Write descriptions for issue?")
+	flag.Parse()
+	initCli()
+	m.Run()
+}
